2e19341c8a1c90e6a236d6719e94cd939ecc3dcb
Sebastian Kriems <bastoberto@gmx.de>
2024-07-15 13:49:14 +0200

    chore: more types for messages

--- backend/src/types.ts
+++ backend/src/types.ts
@@ -20,4 +20,67 @@ type PongMessage = {
   type: "pong";
 };
 
-export type Message = PingMessage | PongMessage;
+type ErrorMessage = {
+  type: "error";
+  message: string;
+};
+
+type RegisterPlayerMessage = {
+  type: "registerPlayer";
+  name: string;
+};
+
+type RegisteredPlayerMessage = {
+  type: "registeredPlayer";
+  name: string;
+};
+
+type PlayerOnlineMessage = {
+  type: "playerOnline";
+  name: string;
+};
+
+type PlayerOfflineMessage = {
+  type: "playerOffline";
+  name: string;
+};
+
+export type MoveMessage = {
+  type: "move";
+  value: string;
+  matchId: string;
+  playerName: string;
+  board: string[];
+};
+
+type MoveDoneMessage = {
+  type: "moveDone";
+  value: string;
+  matchId: string;
+  playerName: string;
+  board: string[];
+};
+
+type ResultMessage = {
+  type: "result";
+  match: DBMatch;
+  message: string;
+};
+
+type MatchCreatedMessage = {
+  type: "matchCreated";
+  match: DBMatch;
+};
+
+export type Message =
+  | ErrorMessage
+  | MatchCreatedMessage
+  | MoveDoneMessage
+  | MoveMessage
+  | PingMessage
+  | PlayerOfflineMessage
+  | PlayerOnlineMessage
+  | PongMessage
+  | RegisterPlayerMessage
+  | RegisteredPlayerMessage
+  | ResultMessage;
--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -21,4 +21,62 @@ type MatchCreatedMessage = {
   match: DBMatch;
 };
 
-export type Message = PingMessage | PongMessage | MatchCreatedMessage;
+type ErrorMessage = {
+  type: "error";
+  message: string;
+};
+
+type RegisterPlayerMessage = {
+  type: "registerPlayer";
+  name: string;
+};
+
+type RegisteredPlayerMessage = {
+  type: "registeredPlayer";
+  name: string;
+};
+
+type PlayerOnlineMessage = {
+  type: "playerOnline";
+  name: string;
+};
+
+type PlayerOfflineMessage = {
+  type: "playerOffline";
+  name: string;
+};
+
+export type MoveMessage = {
+  type: "move";
+  value: string;
+  matchId: string;
+  playerName: string;
+  board: string[];
+};
+
+type MoveDoneMessage = {
+  type: "moveDone";
+  value: string;
+  matchId: string;
+  playerName: string;
+  board: string[];
+};
+
+type ResultMessage = {
+  type: "result";
+  match: DBMatch;
+  message: string;
+};
+
+export type Message =
+  | ErrorMessage
+  | MatchCreatedMessage
+  | MoveDoneMessage
+  | MoveMessage
+  | PingMessage
+  | PlayerOfflineMessage
+  | PlayerOnlineMessage
+  | PongMessage
+  | RegisterPlayerMessage
+  | RegisteredPlayerMessage
+  | ResultMessage;
