1406633120cea0760ee77cc511e37e6f76d5a364
Sebastian Kriems <bastoberto@gmx.de>
2024-07-15 12:45:10 +0200

    feat: login route

--- frontend/src/routes.ts
+++ frontend/src/routes.ts
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "./view/pages/Home.vue";
+import Login from "./view/pages/Login.vue";
 
 const ROUTE_NAMES = {
   home: "home",
+  login: "login",
 };
 
 const routes = [{ name: ROUTE_NAMES.home, path: "/", component: Home }];
+const routes = [
+  { name: ROUTE_NAMES.home, path: "/", component: Home },
+  { name: ROUTE_NAMES.login, path: "/login", component: Login },
+];
 
 export const router = createRouter({
   history: createWebHistory(),
---
+++ frontend/src/view/pages/Login.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import { store } from "../../store";
+const playerName = ref("");
+</script>
+
+<template>
+  <div class="container">
+    <h1>Login</h1>
+    <form
+      @submit.prevent="
+        store.setPlayerName(playerName);
+        $router.push({ name: 'home' });
+      "
+    >
+      <input
+        type="text"
+        id="player"
+        v-model.trim="playerName"
+        placeholder="Player"
+      />
+      <button type="submit">Login</button>
+    </form>
+  </div>
+</template>
+
+<style scoped lang="css">
+.container {
+  display: flex;
+  flex-direction: column;
+  gap: 1em;
+  align-items: center;
+  height: 100%;
+}
+h1 {
+  text-align: center;
+}
+form {
+  display: flex;
+  gap: 1rem;
+  flex-direction: column;
+  justify-content: center;
+}
+</style>
