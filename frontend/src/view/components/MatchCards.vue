80b07bd5034e1d1edea0fda0a48fcb677b22b5cf
Sebastian Kriems <bastoberto@gmx.de>
2024-07-15 12:25:28 +0200

    feat: primitive MatchCards component

---
+++ frontend/src/view/components/MatchCards.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import { RouterLink } from "vue-router";
+import { store } from "../../store";
+</script>
+
+<template>
+  <div class="cards">
+    <p v-if="store.matches.length === 0">No matches yet</p>
+    <RouterLink
+      v-for="match in store.matches"
+      :key="match.id"
+      :to="{ name: 'match', params: { id: match.id } }"
+    >
+      <div class="card">
+        <h3>{{ match.player_x ?? "?" }} vs. {{ match.player_o ?? "?" }}</h3>
+      </div>
+    </RouterLink>
+  </div>
+</template>
+
+<style scoped>
+.cards {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 1rem;
+}
+
+.card {
+  width: 70vw;
+  padding: 1rem;
+  border-radius: 20px;
+  font-size: 1.2rem;
+  font-weight: bold;
+  background-color: #1d1d1d;
+}
+
+.card em {
+  font-size: 0.9rem;
+}
+</style>
